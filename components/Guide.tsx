d55a29b9e942fcf987ce11da8a6e6d285f701a7f
sunnybhandal <sunnybhandal123@gmail.com>
2023-11-08 18:16:26 -0700

    almost done guide part

---
+++ components/Guide.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import React from "react";
+
+const Guide = () => {
+  return (
+    <section className="flexCenter flex-col">
+      <div className="border-2 border-red-500 padding-container max-container w-full pb-24">
+        <Image src="/camp.svg" alt="camp" width={50} height={50} />
+        <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
+          We are here for you
+        </p>
+        <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">
+            Guiding You Easily
+          </h2>
+          <p className="regular-16 text-gray-30 xl:max-w-[520px]">
+            sndsnkfn sndksn f ks nfdssd s kfnin ighieh gei iheith ietiehtieht
+            ieh iht e
+          </p>
+        </div>
+      </div>
+      <div className="flexCenter max-container relative w-full border-2 border-blue-500">
+        <Image
+          src="/boat.png"
+          alt="boat"
+          width={1440}
+          height={580}
+          className="w-full object-cover object-center 2xl:rounded-5xl"
+        />
+        <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
+          <div className="flexBetween flex-col">
+            <div className="flex w-full flex-col">
+              <div className="flexBetween w-full">
+                <p className="regular-16 text-gray-20">Destination</p>
+                <p className="bold-16 text-green-50">48 min</p>
+              </div>
+              <p className="bold-20 mt-2">Aguas Calientes</p>
+            </div>
+            <div className="flex w-full flex-col">
+              <p className="regular-16 text-gray-20">Start track</p>
+              <h4 className="bold-20 mt-2 whitespace-nowrap">
+                Wonorejo Pasuruan
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Guide;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,35 +1,76 @@
+"use client";
 import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import Button from "./Button";
+import { useState } from "react";
 
 const Navbar = () => {
-  return (
-    <nav className="flexBetween max-container padding-container relative py-5">
-      <Link href="/">
-        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
-      </Link>
-      <ul className="hidden h-full gap-12 lg:flex">
-        {NAV_LINKS.map((link) => (
-          <Link
-            href={link.href}
-            key={link.key}
-            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
-          >
-            {link.label}
-          </Link>
-        ))}
-      </ul>
+  const [mobileMenu, setMobileMenu] = useState(false);
 
-      <div className="lg:flexCenter hidden">
-        <Button
-          type="button"
+  const onClickMobileMenu = () => {
+    return (
+      <div className="relative width-full text-red bg-slate-600">"Hi"</div>
+    );
+  };
+  return (
+    <>
+      <nav className="flexBetween max-container padding-container relative py-5">
+        <Link href="/">
+          <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+        </Link>
+        <ul className="hidden h-full gap-12 lg:flex">
+          {NAV_LINKS.map((link) => (
+            <Link
+              href={link.href}
+              key={link.key}
+              className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+            >
+              {link.label}
+            </Link>
+          ))}
+        </ul>
+
+        <div className="lg:flexCenter hidden">
+          <Button
+            type="button"
+            title="Login"
+            icon="/user.svg"
+            variant="btn_dark_green"
+          />
+        </div>
+
+        <Image
+          src="menu.svg"
+          alt="menu"
           title="Login"
-          icon="/user.svg"
-          variant="btn_dark_green"
+          width={32}
+          height={32}
+          className="inline-block cursor-pointer lg:hidden"
+          onClick={() =>
+            mobileMenu ? setMobileMenu(false) : setMobileMenu(true)
+          }
         />
-      </div>
-    </nav>
+      </nav>
+
+      {mobileMenu ? (
+        <div className="z-1 absolute text-white w-full h-full bg-black">
+          <ul className="h-full flex-reverse gap-12 lg:hidden">
+            {NAV_LINKS.map((link) => (
+              <Link
+                href={link.href}
+                key={link.key}
+                className="regular-16 text-gray-50 flex cursor-pointer pb-1.5 transition-all hover:font-bold"
+              >
+                {link.label}
+              </Link>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 
